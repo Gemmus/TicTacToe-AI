3025e36deda488c3828ef996a04ed2fb432ca9ad
Gemmus <decadentcrown@gmail.com>
2023-05-25 17:48:36 +0300

    Minimax added with additional functions.

--- main.py
+++ main.py
@@ -40,7 +40,6 @@ radius = square_size // 4
 pygame.init()
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("TIC TAC TOE")
-screen.fill(background_colour)
 
                             # *.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.#
                             #                                                   #
@@ -99,7 +98,7 @@ class Board:
 
 
 class AI:
-    def __init__(self, level=0, player=2):
+    def __init__(self, level=1, player=2):
         self.level = level
         self.player = player
 
@@ -108,18 +107,68 @@ class AI:
         indexes = random.randrange(0, len(empty_squares))
         return empty_squares[indexes]  # (row, column)
 
-    def eval(self, main_board):
+    def minimax(self, board, maximizing):
+        ###############################
+        #   Checking Terminal Cases   #
+        ###############################
+        case = board.final_state()
+        # Player1 wins #
+        if case == 1:
+            return 1, None  # evaluation, move
+        # Player2 wins #
+        if case == 2:
+            return -1, None
+        # Draw #
+        elif board.checking_full():
+            return 0, None
+
+        ################################
+        #   Minimizing or Maximizing   #
+        ################################
+        if maximizing:
+            max_evaluation = -100
+            best_move = None
+            empty_squares = board.get_empty_squares()
+
+            for (row, column) in empty_squares:
+                temp_board = copy.deepcopy(board)
+                temp_board.marking_square(row, column, 1)
+                evaluation = self.minimax(temp_board, False)[0]
+                if evaluation > max_evaluation:
+                    max_evaluation = evaluation
+                    best_move = (row, column)
+            return max_evaluation, best_move
+
+        elif not maximizing:
+            min_evaluation = 100
+            best_move = None
+            empty_squares = board.get_empty_squares()
+
+            for (row, column) in empty_squares:
+                temp_board = copy.deepcopy(board)
+                temp_board.marking_square(row, column, self.player)
+                evaluation = self.minimax(temp_board, True)[0]
+                if evaluation < min_evaluation:
+                    min_evaluation = evaluation
+                    best_move = (row, column)
+            return min_evaluation, best_move
+
+    def evaluation(self, main_board):
         if self.level == 0:
             #####################
             #   Random Choice   #
             #####################
+            evaluation = "random"
             move = self.random(main_board)
         else:
             ################################
             #   Minimax Algorithm Choice   #
             ################################
-            pass
-        return move # row, column
+            evaluation, move = self.minimax(main_board, False)
+
+        print(f'AI has chosen to mark the square in position {move} with an evaluation of {evaluation}.')
+
+        return move  # row, column
 
 
 class Game:
@@ -132,6 +181,7 @@ class Game:
         self.show_lines()
 
     def show_lines(self):
+        screen.fill(background_colour)
         # Vertical lines
         pygame.draw.line(screen, line_colour, (square_size, 0), (square_size, height), line_width)
         pygame.draw.line(screen, line_colour, (width - square_size, 0), (width - square_size, height), line_width)
@@ -162,6 +212,20 @@ class Game:
     def next_turn(self):
         self.player = self.player % 2 + 1  # changes player, value between 1 and
 
+    def make_move(self, row, column):
+        self.board.marking_square(row, column, self.player)
+        self.draw_figure(row, column)
+        self.next_turn()
+
+    def change_game_mode(self):
+        self.game_mode = 'ai' if self.game_mode == 'pvp' else 'pvp'
+
+    def reset(self):
+        self.__init__()
+
+    def game_over(self):
+        return self.board.final_state() != 0 or self.board.checking_full()
+
                             # *.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.#
                             #                                                   #
                             #                   MAIN PROGRAMME                  #
@@ -186,29 +250,42 @@ def main():
                 pygame.quit()
                 sys.exit()
 
+            if event.type == pygame.KEYDOWN:
+                # Changing Game Mode #
+                if event.key == pygame.K_g:
+                    game.change_game_mode()
+
+                # Changing AI Level #
+                if event.key == pygame.K_0 or event.key == pygame.K_KP_0:
+                    ai.level = 0
+                if event.key == pygame.K_1 or event.key == pygame.K_KP_1:
+                    ai.level = 1
+
+                # Restart #
+                if event.key == pygame.K_r:
+                    game.reset()
+                    board = game.board
+                    ai = game.ai
+
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = event.pos
                 row = pos[1] // square_size
                 column = pos[0] // square_size
-                print(row, column)
 
-                if board.empty_square(row, column):
-                    board.marking_square(row, column, game.player)
-                    print(board.squares)
-                    game.draw_figure(row, column)
-                    game.next_turn()
-                    print(board.squares)
+                if board.empty_square(row, column) and game.running:
+                    game.make_move(row, column)
 
-        if game.game_mode == 'ai' and game.player == ai.player:
+                    if game.game_over():
+                        game.running = False
+
+        if game.game_mode == 'ai' and game.player == ai.player and game.running:
             pygame.display.update()
 
-            row, column = ai.eval(board)
+            row, column = ai.evaluation(game.board)
+            game.make_move(row, column)
 
-            board.marking_square(row, column, game.player)
-            print(board.squares)
-            game.draw_figure(row, column)
-            game.next_turn()
-            print(board.squares)
+            if game.game_over():
+                game.running = False
 
         pygame.display.update()
 
