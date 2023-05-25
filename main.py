ab28f81b9cdf4a7bdf192a06aa5c721bbe9fa3e9
Gemmus <decadentcrown@gmail.com>
2023-05-25 14:24:18 +0300

    Clicked square's position registered.

--- main.py
+++ main.py
@@ -1,40 +1,89 @@
 import sys
 import pygame
-import random
 import numpy as np
+import random
 import copy
 
-#################
-#   Variables   #
-#################
+                            # *.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.#
+                            #                                                   #
+                            #                     VARIABLES                     #
+                            #                                                   #
+                            # *.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*:*.*.*.*.*.*.*.*.*.#
 
-WIDTH = 600
-HEIGHT = 600
+width = 600
+height = 600
+background_colour = (247, 243, 234)
 
-# Colours:
-BACKGROUND_COLOUR = (247, 243, 234)
+rows = 3
+columns = 3
 
+square_size = width // columns
 
-###########################
-#   Setting up the Game   #
-###########################
+line_colour = (82, 82, 75)
+line_width = 15
+
+                            # *.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.#
+                            #                                                   #
+                            #               SETTING UP THE GAME                 #
+                            #                                                   #
+                            # *.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*:*.*.*.*.*.*.*.*.*.#
 
 pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("TIC TAC TOE")
-screen.fill(BACKGROUND_COLOUR)
+screen.fill(background_colour)
+
+                            # *.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.#
+                            #                                                   #
+                            #                      CLASSES                      #
+                            #                                                   #
+                            # *.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*:*.*.*.*.*.*.*.*.*.#
 
 
-######################
-#   Main Programme   #
-######################
+class Board:
+    def __init__(self):
+        self.squares = np.zeros((rows, columns))
+        print(self.squares)
+
+    def marking_square(self, row, column, player):
+        self.squares[row][column] = player
+
+
+class Game:
+    def __init__(self):
+        self.board = Board()
+        self.player = 1
+        self.show_lines()
+
+    def show_lines(self):
+        # Vertical lines
+        pygame.draw.line(screen, line_colour, (square_size, 0), (square_size, height), line_width)
+        pygame.draw.line(screen, line_colour, (width - square_size, 0), (width - square_size, height), line_width)
+        # Horizontal lines
+        pygame.draw.line(screen, line_colour, (0, square_size), (width, square_size), line_width)
+        pygame.draw.line(screen, line_colour, (0, height - square_size), (width, height - square_size), line_width)
+
+                            # *.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.#
+                            #                                                   #
+                            #                   MAIN PROGRAMME                  #
+                            #                                                   #
+                            # *.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*:*.*.*.*.*.*.*.*.*.#
+
 
 def main():
+
+    Game()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = event.pos
+                row = pos[1] // square_size
+                column = pos[0] // square_size
+                print(row, column)
 
         pygame.display.update()
 
