38d7033b09bac43ff84e862821cb2cb0a0ec63b4
Gemmus <decadentcrown@gmail.com>
2023-05-25 13:47:26 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import pygame
+import random
+import numpy as np
+import copy
+import sys
+
+#################
+#   Variables   #
+#################
+
+WIDTH = 600
+HEIGHT = 600
+
+# Colours:
+BACKGROUND_COLOUR = (247, 243, 234)
+
+###########################
+#   Setting up the Game   #
+###########################
+
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("TIC TAC TOE")
+screen.fill(BACKGROUND_COLOUR)
+
+
+def main():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
+
+        
+main()
+
